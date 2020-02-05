0c1f72825d9ce5f800da78e88265ae3660e69703
Supachai Chaimangua (Tor) <tortechnocom@gmail.com>
2020-02-05 22:20:51 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# AT_SIM7020E
+
+### AT Commands for Simcom SIM7020E
+
+##### Author: Supachai Chaimangua (Tor)
+##### Organization: Davin Technology Co., Ltd.
+##### Created Date: 2020-02-05T22:11+07:00
+##### Updated Date: 2020-02-05T22:11+07:00
---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+
+AT_SIM7020E v1.0.0
+
+Author: Supachai Chaimangua (Tor)
+Organization: Davin Technology Co., Ltd.
+Created Date: 2020-02-05T22:11+07:00
+Updated Date: 2020-02-05T22:11+07:00
+
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
